3ba765395ef881bbb69ba23b7a0926314f74e08c
Chatrapathi Akula <chatrapathi.akula@gmail.com>
2020-04-26 16:55:36 +0100

    bitarray repo first commit

---
+++ ba.go
@@ -0,0 +1,130 @@
+package bigbitarray
+
+import (
+	"bytes"
+	"math"
+	"math/bits"
+
+	"github.com/bmkessler/fastdiv"
+)
+
+var c64 fastdiv.Uint64
+
+func init() {
+	c64 = fastdiv.NewUint64(64)
+}
+
+type Bit = uint64
+
+const (
+	Zero = Bit(iota)
+	One
+)
+
+// BitArray is an array data structure that compactly stores bits.
+// Bits externally represented as `bool` are stored internally as `uint64`s.
+// The total number of bits stored is set at creation and is immutable.
+type BitArray struct {
+	bits []uint64
+	n    int // no. of bits
+}
+
+// New creates a new BitArray of `n` bits.
+func New(n int) BitArray {
+	return BitArray{
+		bits: make([]uint64, int(math.Ceil(float64(n)/64))),
+		n:    n,
+	}
+}
+
+func biandsi(k int) (uint64, uint64) { return c64.DivMod(uint64(k)) }
+
+func (ba *BitArray) set1(bi, si uint64) { ba.bits[bi] |= 1 << si }
+func (ba *BitArray) set0(bi, si uint64) { ba.bits[bi] &= ^(1 << si) }
+
+// Set sets the bit at position k.
+func (ba *BitArray) Set(k int) { ba.set1(biandsi(k)) }
+
+// SetAll sets all the bits.
+func (ba *BitArray) SetAll() {
+	for i := range ba.bits {
+		ba.bits[i] = math.MaxUint64
+	}
+}
+
+// Clr clears the bit at position k.
+func (ba *BitArray) Clr(k int) { ba.set0(biandsi(k)) }
+
+// ClrAll clears all the bits.
+func (ba *BitArray) ClrAll() {
+	for i := range ba.bits {
+		ba.bits[i] = 0
+	}
+}
+
+func (ba *BitArray) chk(bi, si uint64) bool { return (ba.bits[bi]>>si)&1 > 0 }
+
+// ChkSet returns the value of the bit at position k before setting it.
+func (ba *BitArray) ChkSet(k int) (b bool) {
+	bi, si := biandsi(k)
+	b = ba.chk(bi, si)
+	if !b {
+		ba.set1(bi, si)
+	}
+	return
+
+}
+
+// ChkClr returns the value of the bit at position k before clearing it.
+func (ba *BitArray) ChkClr(k int) (b bool) {
+	bi, si := biandsi(k)
+	b = ba.chk(bi, si)
+	if b {
+		ba.set0(bi, si)
+	}
+	return
+}
+
+// Tgl toggles the bit at position k.
+func (ba *BitArray) Tgl(k int) {
+	bi, si := biandsi(k)
+	ba.bits[bi] ^= 1 << si
+}
+
+// Cnt returns the number of set bits.
+func (ba *BitArray) Cnt() (n int) {
+	for _, b := range ba.bits {
+		n += bits.OnesCount64(b)
+	}
+	return
+}
+
+// Chk returns the value of the bit at position k.
+func (ba *BitArray) Chk(k int) bool {
+	bi, si := biandsi(k)
+	return (ba.bits[bi]>>si)&1 > 0
+}
+
+// Put sets the value of the bit at position k to v.
+func (ba *BitArray) Put(k int, v Bit) {
+	bi, si := biandsi(k)
+	ba.bits[bi] = (ba.bits[bi] & ^(1 << si)) | (v << si)
+}
+
+// Swap swaps the value of bit at position k with v. On return, v contains the old value.
+func (ba *BitArray) Swap(k int, v *Bit) {
+	bi, si := biandsi(k)
+	ob := (ba.bits[bi] >> si) & 1
+	ba.bits[bi] = (ba.bits[bi] & ^(1 << si)) | (*v << si)
+	*v = ob
+}
+
+func (ba *BitArray) String() string {
+	bs := bytes.Repeat([]byte("0"), ba.n)
+	for i := range bs {
+		if ba.Chk(i) {
+			bs[i] = '1'
+		}
+	}
+	return string(bs)
+}
---
+++ ba_test.go
@@ -0,0 +1,228 @@
+package bigbitarray
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+	"time"
+)
+
+func randomize(ba *BitArray, rng *rand.Rand) {
+	for i := 0; i < ba.n; i++ {
+		if rng.Float32() < 0.5 {
+			ba.Tgl(i)
+		}
+	}
+}
+
+func TestNew(t *testing.T) {
+	tests := []struct {
+		n, nbits int
+	}{
+		{4, 1},
+		{64, 1},
+		{65, 2},
+		{256, 4},
+		{257, 5},
+	}
+
+	for i, tt := range tests {
+		ba := New(tt.n)
+		if tt.nbits != len(ba.bits) {
+			t.Fatalf("test %d failed. got = %d, exp = %d\n", i, len(ba.bits), tt.nbits)
+		}
+	}
+}
+
+func TestBitArray(t *testing.T) {
+	t.Run("swap", func(t *testing.T) {
+		ba := New(257)
+		v := One
+		for k := 0; k < ba.n; k++ {
+			ba.Swap(k, &v)
+			if v != Zero && !ba.Chk(k) {
+				t.Fatalf("Test at bit %d failed. got = %d, exp = %d\n", k, One, Zero)
+			}
+		}
+	})
+
+	t.Run("chkset", func(t *testing.T) {
+		ba := New(257)
+		ba.ClrAll()
+		for k := 0; k < ba.n; k++ {
+			if ba.ChkSet(k) != false {
+				t.Fatalf("Test at bit %d failed. got = %t, exp = %t\n", k, true, false)
+			}
+		}
+	})
+
+	t.Run("chkclr", func(t *testing.T) {
+		ba := New(257)
+		ba.SetAll()
+
+		for k := 0; k < ba.n; k++ {
+			if ba.ChkClr(k) != true {
+				t.Fatalf("Test at bit %d failed. got = %t, exp = %t\n", k, false, true)
+			}
+		}
+	})
+
+	t.Run("chk", func(t *testing.T) {
+		eb := "10000100010111111000011100001100111000000111011011110110001101000111100000010001001000110001001101110001010001100010010010101010001100010101101101011101100001111011110100100011100001101001001001110101000010000000011010000010111111111111111100000001110100001"
+		ba := New(len(eb))
+		for k, b := range eb {
+			if b == '1' {
+				ba.Put(k, One)
+			}
+		}
+
+		// test
+		for k, b := range eb {
+			if (b == '1') != ba.Chk(k) {
+				t.Fatalf("Test failed. got = %s\nexp = %s\n", ba.String(), eb)
+			}
+		}
+
+	})
+
+	t.Run("put", func(t *testing.T) {
+		eb := "10000100010111111000011100001100111000000111011011110110001101000111100000010001001000110001001101110001010001100010010010101010001100010101101101011101100001111011110100100011100001101001001001110101000010000000011010000010111111111111111100000001110100001"
+		ba := New(len(eb))
+		for k, b := range eb {
+			if b == '1' {
+				ba.Put(k, One)
+			}
+		}
+		if eb != ba.String() {
+			t.Fatalf("Test failed. got = %s\nexp = %s\n", ba.String(), eb)
+		}
+	})
+
+	t.Run("set and get", func(t *testing.T) {
+		eb := "11010011000010000010001110000010001000111011111010001000001000110011111110000001010111111010000001101110010011000010011100100001110100111110010100101100101000101001110100001101100010001010001001100011100001001111011011001100101011001111100010100000001001111"
+		ba := New(len(eb))
+		for k, b := range eb {
+			if b == '1' {
+				ba.Set(k)
+			}
+		}
+		if eb != ba.String() {
+			t.Fatalf("Test failed. got = %s\nexp = %s\n", ba.String(), eb)
+		}
+	})
+
+	t.Run("count", func(t *testing.T) {
+		eb := "00011100111011110101011110110010010100101010000110001010100011011010111001110110011111001101100000101011010011001110110000111010101100010110001101101011111100110000110101100000000100011110100111000101100001110010010011001001000001111010101110000001001101001"
+		ba := New(len(eb))
+		for k, b := range eb {
+			if b == '1' {
+				ba.Set(k)
+			}
+		}
+		slow_count := func(ba *BitArray) (n int) {
+			for i := 0; i < ba.n; i++ {
+				if ba.Chk(i) {
+					n++
+				}
+			}
+			return
+		}
+
+		ec := slow_count(&ba)
+		gc := ba.Cnt()
+		if gc != ec {
+			t.Fatalf("Test failed. got = %d, exp = %d\n", gc, ec)
+		}
+
+		fmt.Println("gc: ", gc)
+	})
+}
+
+func BenchmarkNew(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		New(256)
+	}
+}
+
+func BenchmarkBitArray(b *testing.B) {
+	b.Run("chk", func(b *testing.B) {
+		b.ReportAllocs()
+		b.StopTimer()
+		ba := New(257)
+		b.StartTimer()
+		for i := 0; i < b.N; i++ {
+			ba.Chk(127)
+		}
+	})
+
+	b.Run("put", func(b *testing.B) {
+		b.ReportAllocs()
+		b.StopTimer()
+		ba := New(257)
+		b.StartTimer()
+		for i := 0; i < b.N; i++ {
+			ba.Put(127, One)
+		}
+	})
+
+	b.Run("set", func(b *testing.B) {
+		b.ReportAllocs()
+		b.StopTimer()
+		ba := New(257)
+		b.StartTimer()
+		for i := 0; i < b.N; i++ {
+			ba.Set(127)
+		}
+	})
+
+	b.Run("tgl", func(b *testing.B) {
+		b.ReportAllocs()
+		b.StopTimer()
+		ba := New(257)
+		b.StartTimer()
+		for i := 0; i < b.N; i++ {
+			ba.Tgl(127)
+		}
+	})
+
+}
+
+func BenchmarkBitArray_Cnt(b *testing.B) {
+	b.Run("cnt - chk", func(b *testing.B) {
+		b.ReportAllocs()
+		b.StopTimer()
+		ba := New(257)
+		randomize(&ba, rand.New(rand.NewSource(time.Now().UnixNano())))
+		cnt := func(ba *BitArray) (n int) {
+			for k := 0; k < ba.n; k++ {
+				if ba.Chk(k) {
+					n++
+				}
+			}
+			return
+		}
+		b.StartTimer()
+		for i := 0; i < b.N; i++ {
+			cnt(&ba)
+		}
+	})
+
+	b.Run("cnt - bits", func(b *testing.B) {
+		b.ReportAllocs()
+		b.StopTimer()
+		ba := New(257)
+		randomize(&ba, rand.New(rand.NewSource(time.Now().UnixNano())))
+		b.StartTimer()
+		for i := 0; i < b.N; i++ {
+			ba.Cnt()
+		}
+	})
+
+}
+
+func BenchmarkBiandSi(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		biandsi(i)
+	}
+}
